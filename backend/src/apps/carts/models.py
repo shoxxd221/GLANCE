20c90ed77b84bd5054597c15693c8c7157d59578
shoxxd221 <shoxxd@mail.ru>
2024-08-14 22:58:50 +0300

    Added cart model

---
+++ backend/src/apps/carts/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+from apps.goods.models import Goods
+
+
+class Cart(models.Model):
+    """Модель корзины"""
+    user = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        verbose_name='Пользователь, которому соответсвует товар в корзине'
+    )
+    goods = models.ForeignKey(
+        Goods,
+        on_delete=models.CASCADE,
+        verbose_name='Товар, который пользователь добавил в корзину'
+    )
