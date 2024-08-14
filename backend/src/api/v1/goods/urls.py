e82c9a076c4a26f1af711584e369348b84cdb3b4
shoxxd221 <shoxxd@mail.ru>
2024-08-14 20:52:48 +0300

    Added viewset for goods

--- backend/src/api/v1/goods/urls.py
+++ backend/src/api/v1/goods/urls.py
@@ -2,12 +2,14 @@ from django.urls import include, path
 
 from rest_framework.routers import SimpleRouter
 
-from api.v1.goods.views import CategoryViewSet
+from api.v1.goods.views import CategoryViewSet, GoodsViewSet
 
 
 router = SimpleRouter()
 
 router.register(r'category', CategoryViewSet)
+router.register(r'goods', GoodsViewSet)
+
 
 urlpatterns = [
     path('', include(router.urls)),
