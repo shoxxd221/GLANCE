981b6e2ad101d57b4a9cb692aa34800a58041459
shoxxd221 <shoxxd@mail.ru>
2024-12-20 21:50:32 +0300

    Added prod throttling settings

--- backend/src/config/settings/prod.py
+++ backend/src/config/settings/prod.py
@@ -2,3 +2,13 @@ from .base import *
 
 
 DEBUG = False
+
+REST_FRAMEWORK['DEFAULT_THROTTLE_CLASSES'] = [
+    'rest_framework.throttling.AnonRateThrottle',
+    'rest_framework.throttling.UserRateThrottle',
+]
+
+REST_FRAMEWORK['DEFAULT_THROTTLE_RATES'] = {
+    'anon': '100/day',
+    'user': '1000/day',
+}
